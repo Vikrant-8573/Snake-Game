d66a613f80f459db22504c02ce320a1b365ef816
Vikrant <nwalterrook123@gmail.com>
2019-10-28 18:27:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+let app = express();
+
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.sendFile('web.html', {
+        root: './views'
+    });
+});
+
+app.listen(5000, () => {
+    console.log('Server started at port 5000');
+});
